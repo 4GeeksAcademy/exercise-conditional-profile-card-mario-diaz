4287e30401d04d1d1f901488f9c9a49dcef4fb74
MarioDiaz9 <pindadodiazmario@gmail.com>
2026-05-22 10:15:48 +0000

    Second

--- src/js/app.js
+++ src/js/app.js
@@ -38,10 +38,24 @@ function render(variables = {}) {
     variables.country ? variables.country : "Country"
   }</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com{variables.twitter ? variables.twitter : ""}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com{variables.github ? variables.github : ""}"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com{variables.linkedin ? variables.linkedin : ""}"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com{variables.instagram ? variables.instagram : ""}"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="${
+              variables.twitter
+                ? "https://twitter.com/" + variables.twitter
+                : "#"
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${
+              variables.github ? "https://github.com/" + variables.github : "#"
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="${
+              variables.linkedin
+                ? "https://linkedin.com/in/" + variables.linkedin
+                : "#"
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${
+              variables.instagram
+                ? "https://instagram.com/" + variables.instagram
+                : "#"
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
